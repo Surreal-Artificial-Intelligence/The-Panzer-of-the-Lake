8be57b4390f9470acf35ef425f418ca4bf103ad0
Emile-J <jonkersemile@protonmail.com>
2024-09-23 22:58:50 +0200

    fix(Azure): General fixes and improvements for the Azure Integrations

--- src/data_class/retry_policy.py
+++ src/data_class/retry_policy.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 
 @dataclass
 class RetryPolicy:
-    max_retries: int = 10
+    max_retries: int = 5
     initial_delay: int = 1
     backoff_factor: int = 2
     jitter: float = 0.1
